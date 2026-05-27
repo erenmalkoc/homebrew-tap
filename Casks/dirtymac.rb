e6a23ff89e5e354c09df73ce2fe6d08fdd651e73
Eren Malkoç <malkoceren24@gmail.com>
2026-05-27 16:27:40 +0300

    Update dirtymac to version 1.1.1

--- Casks/dirtymac.rb
+++ Casks/dirtymac.rb
@@ -1,6 +1,6 @@
 cask "dirtymac" do
-  version "1.1.0"
-  sha256 "d71d67dcb19cf3cf65c0eb49f40e958b15198b616a1633a4f7a9aa1659c658d6"
+  version "1.1.1"
+  sha256 "e97b5b3f5a19d2a4218a401779ace9e1fbd9f617622f807aabc3c65257347a35"
 
   url "https://github.com/erenmalkoc/dirtymac/releases/download/v#{version}/dirtymac-#{version}.dmg"
   name "dirtymac"
@@ -12,7 +12,7 @@ cask "dirtymac" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :tahoe"
+  depends_on macos: ">= :sonoma"
 
   app "dirtymac.app"
 
